709834496af1cfb51a58a6a29f74a0483fbbc9b9
Terry Zhou <Terry.Zhou@synaptics.com>
2024-01-08 16:55:54 +0800

    syna: myna2: net: enable ALTDESCRIPTOR feature
    Change-Id: I47ceb715731f4e8e65727a45602d6f972f786ed0

--- drivers/net/designware.h
+++ drivers/net/designware.h
@@ -11,6 +11,9 @@
 #include <asm-generic/gpio.h>
 #endif
 
+#if defined(CONFIG_TARGET_MYNA2)
+#define CONFIG_DW_ALTDESCRIPTOR 1
+#endif
 #define CONFIG_TX_DESCR_NUM	16
 #define CONFIG_RX_DESCR_NUM	16
 #define CONFIG_ETH_BUFSIZE	2048
